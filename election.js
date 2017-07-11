98ebece73c5b690536c121a84eb070980ef30185
Lia Forgione <liaforg@gmail.com>
2017-07-11 15:35:17 -0400

    completed fix

--- election.js
+++ election.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  var summonCandidates = document.querySelectorAll('responseData.candidates')
 
+  runSummonCandidates = document.addEventListener('click', function(event) {
+    console.log('click');
+
+    $.ajax({
+      url: 'https://bb-election-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'json',
+    }).done(function (responseData){
+        console.log(responseData);
+        for(var i = 0; i < responseData.candidates.length; i++) {
+          var element = document.createElement('div');
+          element.innerHTML = 'Name: ' + responseData.candidates[i].name + "<br>" + ' Votes:  ' + responseData.candidates[i].votes + "<br>" + "<br>";
+          document.querySelector('#list').append(element);
+        };
+      });
+
+    });
 });
